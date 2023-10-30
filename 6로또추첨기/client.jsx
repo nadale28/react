d1fc8522333b18f6a87054f564a09ee1bd5d1129
nadale20 <nadale20@gmail.com>
2023-10-30 21:14:19 +0900

    로또 추첨기 공부

---
+++ 6로또추첨기/client.jsx
@@ -0,0 +1,6 @@
+import Lotto from "./Lotto.jsx";
+
+const React = require('react');
+const ReactDom = require('react-dom/client');
+
+ReactDom.createRoot(document.querySelector('#root')).render(<Lotto/>);
